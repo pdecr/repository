a5f71fb85b7d19625ab6d6ab88a84194a1bb4fdb
Externe Formation <ext.formation@d2sim7.loc>
2018-10-08 15:11:33 +0200

    add subnet and igw

--- vpc/main.tf
+++ vpc/main.tf
@@ -11,3 +11,20 @@ resource "aws_vpc" "IAC" {
     Name = "IAC tooling"
   }
 }
+
+resource "aws_subnet" "IAC_subnet1" {
+  vpc_id     = "${aws_vpc.IAC.id}"
+  cidr_block = "172.23.1.0/24"
+
+  tags {
+    Name = "IAC_subnet1"
+  }
+}
+
+resource "aws_internet_gateway" "igw_IAC" {
+  vpc_id = "${aws_vpc.IAC.id}"
+
+  tags {
+    Name = "igw_IAC"
+  }
+}
