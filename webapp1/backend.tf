588d12fbd5ce4a16256099cb2b3b821449141895
Externe Formation <ext.formation@d2sim7.loc>
2018-10-10 12:20:27 +0200

    add ELB and a second instance

--- webapp1/backend.tf
+++ webapp1/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "bucket-iac-terraform"
-    key    = "webapp1/terraform.tfstate"
-    region = "eu-west-1"
-    dynamodb_table="tfstate"
+    bucket         = "bucket-iac-terraform"
+    key            = "webapp1/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "tfstate"
   }
 }
--- webapp1/main.tf
+++ webapp1/main.tf
@@ -1,18 +1,17 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-1"
-} 
+}
 
 module "prodwebapp" {
-
   source = "../module/webapp"
-  
-region ="eu-west-1"
-subnet_selected_name=["IAC_subnet2-0"]
-vpc_selected_name=["IAC tooling2"]
-ami_ubuntu=["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-instance_name="webInstance2"
-key_pair="key_pair"
-instance_type="t2.micro"
 
+  region               = "eu-west-1"
+  subnet_selected_name = ["IAC_subnet2-0", "IAC_subnet2-1"]
+  vpc_selected_name    = ["IAC tooling2"]
+  ami_ubuntu           = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  instance_name        = "webInstance2"
+  key_pair             = "key_pair"
+  instance_type        = "t2.micro"
+  elb_tag              = "elbweb"
 }
